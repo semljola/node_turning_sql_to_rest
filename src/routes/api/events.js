fbec43aaa3ec55d64ac11847d6d1ee81f5fe8aa5
Johan Lauri <johl@FL599.local>
2019-10-16 14:42:09 +0200

    Adds auth, vue and views

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -1,17 +1,23 @@
 "use strict";
 
+const boom = require( "boom" );
+
 module.exports.register = async server => {
    server.route( {
        method: "GET",
        path: "/api/events",
        config: {
+           auth: {
+               strategy: "session",
+               mode: "required"
+           },
            handler: async request => {
                try {
                    // get the sql client registered as a plugin
                    const db = request.server.plugins.sql.client;
 
-                   // TODO: Get the current authenticate user's ID
-                   const userId = "user1234";
+                   // get the current authenticated user's id
+                   const userId = request.auth.credentials.profile.id;
 
                    // execute the query
                    const res = await db.events.getEvents( userId );
@@ -19,7 +25,57 @@ module.exports.register = async server => {
                    // return the recordset object
                    return res.recordset;
                } catch ( err ) {
-                   console.log( err );
+                   server.log( [ "error", "api", "events" ], err );
+                   return boom.boomify( err );
+               }
+           }
+       }
+   } );
+
+   server.route( {
+       method: "POST",
+       path: "/api/events",
+       config: {
+           auth: {
+               strategy: "session",
+               mode: "required"
+           },
+           handler: async request => {
+               try {
+                   const db = request.server.plugins.sql.client;
+                   const userId = request.auth.credentials.profile.id;
+                   const { startDate, startTime, endDate, endTime, title, description } = request.payload;
+                   const res = await db.events.addEvent( { userId, startDate, startTime, endDate, endTime, title, description } );
+                   return res.recordset[ 0 ];
+               } catch ( err ) {
+                   server.log( [ "error", "api", "events" ], err );
+                   return boom.boomify( err );
+               }
+           }
+       }
+   } );
+
+   server.route( {
+       method: "DELETE",
+       path: "/api/events/{id}",
+       config: {
+           auth: {
+               strategy: "session",
+               mode: "required"
+           },
+           response: {
+               emptyStatusCode: 204
+           },
+           handler: async request => {
+               try {
+                   const id = request.params.id;
+                   const userId = request.auth.credentials.profile.id;
+                   const db = request.server.plugins.sql.client;
+                   const res = await db.events.deleteEvent( { id, userId } );
+                   return res.rowsAffected[ 0 ] === 1 ? "" : boom.notFound();
+               } catch ( err ) {
+                   server.log( [ "error", "api", "events" ], err );
+                   return boom.boomify( err );
                }
            }
        }
