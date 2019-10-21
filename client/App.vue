aad4cfb3a561a5cbd4138d49b79f7fd89894b8a7
Johan Lauri <johl@FL599.local>
2019-10-21 14:43:23 +0200

    Changes to vue views2

--- client/App.vue
+++ client/App.vue
@@ -1,90 +1,74 @@
 <template>
  <div id="app">
    <h1>{{ msg }}</h1>
-   <div class="row" id="customerList">
-       <h2>List of Quant's List</h2>
-       <table v-if="hasCustomers">
-           <thead>
-               <tr>
-                <th>KundeNr</th>
-                <th>KundeNavn</th>
-                <th>FR_Adresse</th>
-                <th>Kontakt</th>
-                <th> RecDato</th>
-                <th>IdBaksystem</th>
-                <th>ePostAdresse</th>
-                <th>Firmakunde</th>
-               </tr>
-           </thead>
-           <tbody>
-                <tr v-for="customer in customers" :key="customer.KundeNr">
-                <th>{{customer.KundeNr}}</th>
-                <th>{{customer.KundeNavn}}</th>
-                <th>{{customer.FR_Adresse}}</th>
-                <th>{{customer.Kontakt}}</th>
-                <th>{{customer.RecDato}}</th>
-                <th>{{customer.IdBaksystem}}</th>
-                <th>{{customer.ePostAdresse}}</th>
-                <th>{{customer.Firmakunde}}</th>
-
-               </tr>
-           </tbody>
-       </table>
-       <p v-if="noCustomers">No events yet!</p>
-   </div>
-  <div class="row" id="invoiceEdit">
-       <h2>Select company and start & end date for invoice</h2>
-       <form class="col s12" @submit.prevent="getMaterial">
+  <div class="gg-card post no-image" id="invoiceEdit">
+       <h2>1. Specify input details</h2>
+       <form class="col s12" @submit.prevent="getInvoiceDetail">
            <div class="row">
                <div class="input-field col s6">
+                 <span class="datetime-label">Select client</span>
+                 <select v-if="hasCustomers" v-model="selected" style="display:inline">
+                  <option v-for="customer in customers" v-bind:key="customer.KundeNr">
+                    {{ customer.KundeNavn }} [KundeNr:{{ customer.KundeNr }}, Kontakt:{{ customer.Kontakt }}]
+                  </option>
+                </select>
+              </div> 
+               <div class="input-field col s3">
                    <span class="datetime-label">Start Date</span>
                    <datetime v-model="startDate" input-id="startDate" type="date" value-zone="local" input-class="validate"></datetime>
-                   <!-- <label for="startDate" class="datetime-label">Start Date</label> -->
                </div>
-               <div class="input-field col s6">
+               <div class="input-field col s3">
                    <span class="datetime-label">End Date</span>
                    <datetime v-model="endDate" input-id="endDate" type="date" value-zone="local" input-class="validate"></datetime>
-                   <!-- <label for="endDate">End Date</label> -->
                </div>
            </div>
-           <button id="materialSubmit" class="btn" type="submit"><i class="material-icons right">send</i>Submit</button>
-
+           <button id="InvoiceDetailSubmit" class="btn" type="submit"><i class="material-icons right">send</i>Submit</button>
        </form>
   </div>
 
-   <div class="row" id="materialList">
-       <h2>Material</h2>
-       <table v-if="hasMaterial">
+   <div class="row" id="hoursList">
+       <h2 v-if="hasHours">Hours</h2>
+       <table v-if="hasHours" class="striped">
            <thead>
                <tr>
-                <th>KundeNr</th>
-                <th>KundeNavn</th>
-                <th>FR_Adresse</th>
-                <th>Kontakt</th>
-                <th> RecDato</th>
-                <th>IdBaksystem</th>
-                <th>ePostAdresse</th>
-                <th>Firmakunde</th>
+                <th>IdOrdre</th>
+                <th>Info</th>
+                <th>Antall</th>
                </tr>
            </thead>
            <tbody>
-                <tr v-for="customer in customers" :key="customer.KundeNr">
-                <th>{{customer.KundeNr}}</th>
-                <th>{{customer.KundeNavn}}</th>
-                <th>{{customer.FR_Adresse}}</th>
-                <th>{{customer.Kontakt}}</th>
-                <th>{{customer.RecDato}}</th>
-                <th>{{customer.IdBaksystem}}</th>
-                <th>{{customer.ePostAdresse}}</th>
-                <th>{{customer.Firmakunde}}</th>
-
+                <tr v-for="hour in hours" :key="hour.IdOrdre">
+                <th>{{hour.IdOrdre}}</th>
+                <th>{{hour.Info}}</th>
+                <th>{{hour.Antall}}</th>
                </tr>
            </tbody>
        </table>
-       <p v-else>No material yet!</p>
    </div>
 
-
+   <div class="row" id="materialList">
+       <h2 v-if="hasMaterial">Material</h2>
+       <table v-if="hasMaterial">
+           <thead>
+               <tr>
+                <th>IdOrdre</th>
+                <th>Dato</th>
+                <th>Beskrivelse</th>
+                <th>Antall</th>
+                <th>Pris</th>
+               </tr>
+           </thead>
+           <tbody>
+                <tr v-for="theMaterial in material" :key="theMaterial.IdOrdre">
+                <th>{{theMaterial.IdOrdre}}</th>
+                <th>{{theMaterial.Dato}}</th>
+                <th>{{theMaterial.Beskrivelse}}</th>
+                <th>{{theMaterial.Antall}}</th>
+                <th>{{theMaterial.Pris}}</th>
+               </tr>
+           </tbody>
+       </table>
+   </div>
 
  </div>
 </template>
@@ -100,48 +84,110 @@ export default {
    hasCustomers() {
      return this.isLoading === false && this.customers.length > 0;
    },
+   hasMaterial() {
+     return this.isLoading === false && this.material.length > 0;
+   },
+   hasHours() {
+     return this.isLoading === false && this.hours.length > 0;
+   },
    noCustomers() {
      return this.isLoading === false && this.customers.length === 0;
    }
  },
  data() {
    return {
-     isLoading: true,
-        KundeNr: "",
-        KundeNavn: "",
-        FR_Adresse: "",
-        Kontakt: "",
-        RecDato: "",
-        IdBaksystem: "",
-        ePostAdresse: "",
-        Firmakunde: "",
+    isLoading: true,
+    customers:[],
+    material:[],
+    hours:[],
+    selected: '',
+    sortColumn: ''
    };
  },
  methods: {
+   "sortTable": function sortTable(col) {
+     console.log('sort');
+      if (this.sortColumn === col) {
+        this.ascending = !this.ascending;
+      } else {
+        this.ascending = true;
+        this.sortColumn = col;
+      }
+
+      var ascending = this.ascending;
+
+      this.rows.sort(function(a, b) {
+        if (a[col] > b[col]) {
+          return ascending ? 1 : -1
+        } else if (a[col] < b[col]) {
+          return ascending ? -1 : 1
+        }
+        return 0;
+      })
+    },
+    getInvoiceDetail(){
+      this.getHours();
+      this.getMaterial();
+    },
     getMaterial() {
-     const material = {
+       const myInput = {
+       clientId:this.selected,
        startDate: this.startDate ? moment( this.startDate ).format( "YYYY-MM-DD" ) : null,
        endDate: this.endDate ? moment( this.endDate ).format( "YYYY-MM-DD" ) : null,
-       KundeNr: this.KundeNr
      };
      axios
-       .post( "c", event )
-       .then( () => {
-         this.startDate = "";
-         this.endDate = "";
-         this.KundeNr = "";
-         //this.loadEvents();
+       .post( "/api/material", myInput )
+       .then( res => {
+         this.isLoading = false;
+         this.material = this.formatMaterial( res.data );
        } )
        .catch( err => {
          this.msg = err.message;
          console.log( err );
        } );
    },
+    getHours() {
+       const myInput = {
+       clientId:this.selected,
+       startDate: this.startDate ? moment( this.startDate ).format( "YYYY-MM-DD" ) : null,
+       endDate: this.endDate ? moment( this.endDate ).format( "YYYY-MM-DD" ) : null,
+     };
+     axios
+       .post( "/api/hours", myInput )
+       .then( res => {
+         this.isLoading = false;
+         this.hours = this.formatHours( res.data );
+       } )
+       .catch( err => {
+         this.msg = err.message;
+         console.log( err );
+       } );
+   },   
    formatDate( d ) {
      return d ? moment.utc( d ).format( "MMM D, YYYY" ) : "";
    },
    formatTime( t ) {
      return t ? moment( t ).format( "h:mm a" ) : "";
+   },
+   formatHours( hours ) {
+     return hours.map( hour => {
+       return {
+        IdOrdre: hour.IdOrdre,
+        Beskrivelse: hour.Info,
+        Antall: hour.Antall
+       };
+     } );
+   },
+   formatMaterial( material ) {
+     return material.map( theMaterial => {
+       return {
+        IdOrdre: theMaterial.IdOrdre,
+        Dato: this.formatDate( theMaterial.Dato ),
+        Beskrivelse: theMaterial.Beskrivelse,
+        Antall: theMaterial.Antall,
+        Pris: theMaterial.Pris
+       };
+     } );
    },
     formatCustomers( customers ) {
      return customers.map( customer => {
@@ -184,4 +230,9 @@ export default {
  color: #4e9e36;
  font-size: .8rem;
 }
+
+table {
+
+}
+
 </style>
