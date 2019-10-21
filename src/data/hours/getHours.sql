bfd94cc6245c61f289f073d8cb578cd982e33353
Johan Lauri <johl@FL599.local>
2019-10-21 14:41:55 +0200

    Changes to vue views

--- src/data/hours/getHours.sql
+++ src/data/hours/getHours.sql
@@ -3,7 +3,6 @@ FROM [HM_VC_Prod].[dbo].[TidAnnet]
 WHERE  IdOrdre IN (SELECT IdOrdre
     FROM [HM_VC_Prod].[dbo].[Ordre] AS Orders
         INNER JOIN [HM_VC_Prod].[dbo].[Kunde] AS Customers  ON Orders.KundeNr=Customers.KundeNr 
-        WHERE Customers.KundeNr = 28 
-        AND Orders.OrdreDato > '2019-09-01' and Orders.FastPris = 0 and Orders.FerdigDato < '2019-10-19')
-        --AND Orders.OrdreDato > @startDate and Orders.FastPris = 0 and Orders.FerdigDato < @endDate)
+        WHERE Customers.KundeNr = @clientId  
+        AND Orders.OrdreDato > @startDate and Orders.FastPris = 0 and Orders.FerdigDato < @endDate)
 
